be3b91b94c9c61cd12fca6fa482f646595ce1d92
rksorthiya <rsorthiya200@rku.ac.in>
2020-07-30 10:21:09 +0530

    Tutorial 02 Completed.

---
+++ app/src/main/java/com/example/tutorial02/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tutorial02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setTitle(R.string.title);*/
+        /*setTitle("About");*/
+        setContentView(R.layout.activity_main);
+    }
+}
